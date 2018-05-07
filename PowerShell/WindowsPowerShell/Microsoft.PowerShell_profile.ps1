e923e99913572bdf2d04d0620f5c28bde75a57a9
Rune Mønnike <rm@gospoort.com>
2018-05-07 09:53:52 +0200

    Added proxy detection function to Powershell Profile.

--- PowerShell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,10 +7,11 @@
 # Shortcuts to working directories.
 function cdsm { cd "C:\Repos\117-22995 IoT-Structural Monitoring (GIT)\StructuralMonitoring" }
 function cdae { cd "C:\Repos\116-26549 IoT-Air Emissions (GIT)\AirEmissions" }
+function cddh { cd "C:\Repos\117-21220 DataHub" }
 function cdr { cd "C:\Repos" }
 
 # Shortcut to launch Notepad++
-function npp { start "C:\Program Files (x86)\Notepad++\notepad++.exe" @args }
+function npp { start "C:\Program Files\Notepad++\notepad++.exe" @args }
 
 # Quickly move back and forth between visited directories.
 set-alias cd pushd -option AllScope
@@ -22,3 +23,22 @@ Install-GuiCompletion -Key Tab
 # Posh git.
 Import-Module posh-git
 $global:GitPromptSettings.EnableWindowTitle = $null
+
+# Function to autoset proxy
+function proxy {
+	Write-Host "Proxy setting was HTTP=$env:HTTP_PROXY and HTTPS=$env:HTTPS_PROXY"
+
+	if (Test-NetConnection proxy1.ft.corp) {
+		Write-Host "Proxy detected"
+
+		$env:HTTP_PROXY = "http://proxy1.ft.corp:3128"
+		$env:HTTPS_PROXY = "http://proxy1.ft.corp:3128"
+	} else {
+		Write-Host "Proxy not detected"
+
+		Remove-Item Env:\HTTP_PROXY
+		Remove-Item Env:\HTTPS_PROXY
+	}
+
+	Write-Host "Proxy setting is HTTP=$env:HTTP_PROXY and HTTPS=$env:HTTPS_PROXY"
+}
